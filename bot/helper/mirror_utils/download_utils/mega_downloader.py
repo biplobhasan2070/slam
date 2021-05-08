24ec841c893e6fffc5c22d00705efac88d1e8366
AYUSH <81183831+ayushteke@users.noreply.github.com>
2021-05-08 11:13:32 +0530

    Update mega_downloader.py

--- bot/helper/mirror_utils/download_utils/mega_downloader.py
+++ bot/helper/mirror_utils/download_utils/mega_downloader.py
@@ -120,7 +120,7 @@ class MegaAppListener(MegaListener):
 
     def cancel_download(self):
         self.is_cancelled = True
-        self.listener.onDownloadError("<b>☆ Download Stopped By User ☆</b>")
+        self.listener.onDownloadError("<b>💫 Download Stopped By User 💫<b>")
 
 
 class AsyncExecutor:
